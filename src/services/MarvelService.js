6eb4283e3c34c0fe3df84c31a7c5208fc37bb964
Tatiana <tvtugova1511@gmail.com>
2024-05-07 19:44:10 +0300

    Налажено появление данных о случайном персонаже

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,22 +4,36 @@ class MarvelService {
     _apiKey = 'apikey=2aaad9d37131a1f24490695d27b88819';
 
     getResource = async (url) => {
-      let res = await fetch(url);
+        let res = await fetch(url);
   
-      if (!res.ok) {
+        if (!res.ok) {
         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-      }
+        }
   
-      return await res.json();
+        return await res.json();
     };
   
-    getAllCharacters = () => {
-      return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     };
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
     };
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description 
+                ?  (char.description.length > 210 ? `${char.description.slice(0, 210)}…` : char.description)
+                : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
+    }
 }
   
 export default MarvelService;
