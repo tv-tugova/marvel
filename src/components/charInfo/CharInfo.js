a239b66dea87f3df26fea0ba9c6ac2d707a6a716
Tatiana <tvtugova1511@gmail.com>
2024-05-12 14:09:32 +0300

    Установлены предохранители

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -81,7 +81,7 @@ const View = ({char}) => {
     if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
         imgStyle = {'objectFit' : 'contain'};
     }
-    
+
     return (
         <>
             <div className="char__basics">
