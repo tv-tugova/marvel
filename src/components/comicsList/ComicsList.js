92ddc45ba409cb9d27e5f7ac219591d2b7f4cb52
Tatiana <tvtugova1511@gmail.com>
2024-06-01 20:49:18 +0300

    Добавлены Page404 и отдельная страница для каждого комикса

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -47,11 +48,11 @@ const ComicsList = (props) => {
                 <li className="comics__item" 
                     key={i}
                 >
-                    <a href="#">
+                    <Link to={`/comics/${item.id}`}>
                         <img src={item.thumbnail} alt={item.title} style={imgStyle} className="comics__item-img"/>
                         <div className="comics__item-name">{item.title}</div>
                         <div className="comics__item-price">{item.price}</div>
-                    </a>
+                    </Link>
                 </li>
             );
         });
